9feae2e1c79c86fb9ece17c84b0b05073fc0ad90
Jack Atkerson <jatkerson18@gmail.com>
2022-07-30 12:45:17 -0500

    Warehouse model

--- models/index.js
+++ models/index.js
@@ -1,15 +1,29 @@
+const Warehouse = require('./Warehouse');
 const Category = require('./Category');
 const Product = require('./Product');
-
-Product.belongsTo(Category, {
-    foreignKey: 'category_id'
-});
 
 Category.hasMany(Product, {
     foreignKey: 'category_id'
 });
 
+Product.belongsTo(Category, {
+    foreignKey: 'category_id'
+});
+
+Warehouse.hasMany(Category, {
+    foreignKey: 'warehouse_id'
+});
+
+Warehouse.hasMany(Product, {
+    foreignKey: 'warehouse_id'
+});
+
+Category.belongsTo(Warehouse, {
+    foreignKey: 'warehouse_id'
+});
+
 module.exports = {
+    Warehouse,
     Category,
     Product
 };
