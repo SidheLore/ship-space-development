65bb2e1ef7b11521d8f69ebce112e580d5bc09f4
Jack Atkerson <jatkerson18@gmail.com>
2022-07-31 10:21:09 -0500

    Updated warehouse seed

--- seeds/warehouse-seeds.js
+++ seeds/warehouse-seeds.js
@@ -1,1 +1,12 @@
 const { Warehouse } = require('../models')
+
+const warehouseData = [
+    {
+        warehouse_name: 'Coffee Warehouse',
+        description: 'A coffee roasters warehouse, containing coffee, tea, and related equipment.'
+    }
+];
+
+const seedWarehouses = () => Warehouse.bulkCreate(warehouseData);
+
+module.exports = seedWarehouses;
